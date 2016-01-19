c1b5e659331ca0aeefad3b1a39de1527920156c2
Naj <rajanambu@gmail.com>
2016-01-19 20:25:42 +0800

    add task 2

---
+++ task002.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = 'Naj';
+$age = '26';
+$height = '1.70';
+
+
+// print_r($name);
+// print_r($age);
+// print_r($height);
+
+// var_dump($name);
+// var_dump($age);
+// var_dump($height);
+
+$ageInt = (int) $age;
+$heightDouble = (double) $height;
+
+var_dump($ageInt);
+var_dump($heightDouble);
